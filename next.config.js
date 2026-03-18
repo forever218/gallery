5dedb3216450ad0a52ee33d6c7d98868bc55e6aa
forever218 <3316703158@qq.com>
2026-03-18 22:39:39 +0800

    0

--- next.config.js
+++ next.config.js
@@ -6,14 +6,6 @@ const nextConfig = {
     unoptimized: true,
     domains: ['img.2am.top'],
   },
-  // 确保动态路由的 chunk 文件名正确生成
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.output.filename = 'static/chunks/[name]-[contenthash].js';
-      config.output.chunkFilename = 'static/chunks/[name]-[contenthash].js';
-    }
-    return config;
-  },
 }
 
 module.exports = nextConfig
